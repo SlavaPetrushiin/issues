16a3fb80a7ca4b18b0143ebb17afb3fae13e2d2d
petrushin <slava91petrushin@yandex.ru>
2023-11-12 14:40:13 +0300

    added loading page for Issues

---
+++ app/issues/loading.tsx
@@ -0,0 +1,40 @@
+import { Table } from '@radix-ui/themes'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+export default function Loading() {
+	const count = new Array(5).fill("");
+	return (
+		<div>
+
+			<Table.Root variant="surface">
+				<Table.Header>
+					<Table.Row>
+						<Table.ColumnHeaderCell>
+							Title
+						</Table.ColumnHeaderCell>
+						<Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
+						<Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+					</Table.Row>
+				</Table.Header>
+				<Table.Body>
+					{count.map((_, index) => {
+						return (
+							<Table.Row key={index}>
+								<Table.RowHeaderCell>
+									<Skeleton />
+								</Table.RowHeaderCell>
+								<Table.Cell className='hidden md:table-cell'>
+									<Skeleton />
+								</Table.Cell >
+								<Table.Cell className='hidden md:table-cell'>
+									<Skeleton />
+								</Table.Cell>
+							</Table.Row>
+						)
+					})}
+				</Table.Body>
+			</Table.Root>
+		</div>
+	)
+}
