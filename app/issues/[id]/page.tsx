eff96054d1068b47ec6534f57bd1bc930143f735
petrushin <slava91petrushin@yandex.ru>
2023-11-14 21:33:09 +0300

    refactor apply the SRP

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,19 +1,15 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import prisma from '@/prisma/client'
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes'
+import { Box, Grid } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
-import Markdown from 'react-markdown'
-import { Pencil2Icon } from '@radix-ui/react-icons'
-import Link from 'next/link'
-
-
+import IssueDetails from '../_components/IssueDetails'
+import EditIssueButton from "./../_components/EditIssueButton"
 
 const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
 	if (Number.isNaN(+params.id)) notFound()
 
 	const issue = await prisma.issue.findUnique({
 		where: {
-			id: parseInt(params.id)
+		id: parseInt(params.id)
 		}
 	})
 
@@ -22,22 +18,10 @@ const IssueDetailPage = async ({ params }: { params: { id: string } }) => {
 	return (
 		<Grid columns={{ initial: "1", md: "2" }} gap="5">
 			<Box className='space-y-3'>
-				<Heading as='h2' className='capitalize'>{issue.title}</Heading>
-				<Flex gap={"3"}>
-					<IssueStatusBadge status={issue.status} />
-					<Text>{issue.createdAt.toLocaleDateString()}</Text>
-				</Flex>
-				<Card >
-					<Markdown>{issue.description}</Markdown>
-				</Card>
+				<IssueDetails issue={issue} />
 			</Box>
 			<Box>
-				<Button>
-					<Pencil2Icon />
-					<Link href={`/issues/${issue.id}/edit`}>
-						Edit Issue
-					</Link>
-				</Button>
+				<EditIssueButton  issueId={issue.id} />
 			</Box>
 		</Grid>
 	)
