9d3b63093e3b1bc75e60a6d8ca5a9eb4c52a0744
Slava <38832822+SlavaPetrushiin@users.noreply.github.com>
2023-11-29 21:47:18 +0300

    fix error prisma

--- app/configs/authOptions.ts
+++ app/configs/authOptions.ts
@@ -1,13 +1,18 @@
+import prisma from '@/prisma/client';
 import { AuthOptions } from 'next-auth';
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import GoogleProvider from "next-auth/providers/google";
 
 
 export const authOptions: AuthOptions = {
-	// adapter: PrismaAdapter(prisma),
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID!,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
 		})
 	],
+	adapter: PrismaAdapter(prisma),
+	session: {
+		strategy: "jwt",
+	},
 }
