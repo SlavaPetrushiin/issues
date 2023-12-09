11081f808d62c39a234d5d45e88bc2bcd37aa798
Slava <38832822+SlavaPetrushiin@users.noreply.github.com>
2023-12-09 22:56:50 +0300

    added sort in table

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,34 @@
 import prisma from '@/prisma/client'
 import { Table } from '@radix-ui/themes'
 import IssueActions from './_components/IssueActions'
-import { IssueStatusBadge, Link } from '@/app/components';
-import { Status } from '@prisma/client';
+import { IssueStatusBadge, Link as CustomLink } from '@/app/components';
+import { Issue, Status } from '@prisma/client';
+import Link from 'next/link';
+import classNames from 'classnames';
+import { ArrowUpIcon } from '@radix-ui/react-icons';
 
-const IssuesPage = async ({searchParams} :  {searchParams: {status: Status}}) => {
+interface ISearchParamsProps {
+	status: Status;
+	orderBy: IColumn['value']
+}
+
+type OrderByValue = Pick<Issue, 'createdAt' | 'title' | 'status'>;
+interface IColumn {
+	label: string;
+	value: keyof OrderByValue;
+	className?: string;
+}
+
+const IssuesPage = async ({ searchParams }: { searchParams: ISearchParamsProps }) => {
 	const statuses = Object.values(Status);
-	const status = statuses.includes(searchParams.status) 
-		?  searchParams.status
+	const status = statuses.includes(searchParams.status)
+		? searchParams.status
 		: undefined;
+	const columns: IColumn[] = [
+		{ label: "Title", value: "title", className: "" },
+		{ label: "Status", value: "status", className: "hidden md:table-cell" },
+		{ label: "Created", value: "createdAt", className: "hidden md:table-cell" }
+	]
 
 	const issues = await prisma.issue.findMany({
 		where: {
@@ -22,21 +42,30 @@ const IssuesPage = async ({searchParams} :  {searchParams: {status: Status}}) =>
 			<Table.Root variant="surface">
 				<Table.Header>
 					<Table.Row>
-						<Table.ColumnHeaderCell>
-							Title
-						</Table.ColumnHeaderCell>
-						<Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-						<Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+						{
+							columns.map(column => {
+								return (
+									<Table.ColumnHeaderCell key={column.value} className={column.className}>
+										<Link href={{
+											query: { ...searchParams, orderBy: column.value }
+										}}>{column.label}</Link>
+										{column.value === searchParams.orderBy && <ArrowUpIcon className='inline' /> }
+									</Table.ColumnHeaderCell>
+
+								)
+							})
+						}
 					</Table.Row>
+
 				</Table.Header>
 				<Table.Body>
 					{issues.map(issue => {
 						return (
 							<Table.Row key={issue.id}>
 								<Table.RowHeaderCell>
-									<Link href={`/issues/${issue.id}`}>
+									<CustomLink href={`/issues/${issue.id}`}>
 										{issue.title}
-									</Link>
+									</CustomLink>
 									<div className={'block mt-1 md:hidden'}>
 										<IssueStatusBadge status={issue.status} />
 									</div>
